376de66d70f93d0f25a518236b903176f50bbf6f
jms-wandb <jonathan.stanley@wandb.com>
2025-02-28 11:17:11 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,16 @@ variable "aws_region" {
   default = "us-east-2"
 }
 
+variable "namespace" {
+  type        = string
+  description = "Name prefix used for resources"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "Domain name used to access instance."
+}
+
 variable "zone_id" {
   type        = string
   description = "Id of Route53 zone"
